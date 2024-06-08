8bb83990666b020b2699df0c2f13c48b2a8117a1
Jozef Hudec <jojohudec27@gmail.com>
2024-06-08 08:24:23 +0200

    Make creating a document work

--- app/documents/new/page.tsx
+++ app/documents/new/page.tsx
@@ -1,17 +1,20 @@
 'use client'
 
 import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 import { FileUploader } from 'react-drag-drop-files'
 import { toast } from 'react-hot-toast'
+import { useMutation } from '@tanstack/react-query'
 
 import Navbar from '../../components/Navbar/Navbar'
 import withAuth from '../../hoc/with-auth'
 import { fileTypes } from '../../config'
 import useInputs from '../../hooks/useInputs'
 import { changeFormDataFactory } from '../../utils'
+import api from '../../api/axios-instance'
 
 import type { FormEvent } from 'react'
-
+import type { AxiosError } from 'axios'
 import type { NewDocumentFormState } from '../../types/form-types'
 
 function NewDocumentPage() {
@@ -21,6 +24,38 @@ function NewDocumentPage() {
     description: '',
   })
   const [file, setFile] = useState<File | null>(null)
+
+  const router = useRouter()
+
+  const newDocumentMutation = useMutation({
+    mutationFn: async (data: NewDocumentFormState & { file: File }) => {
+      return await api(true).post('/documents', data, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+    },
+    onError: (error: AxiosError) => {
+      const err = error as { response: { data: { errors: { [key: string]: string[] } } } }
+      const errors = err?.response?.data?.errors
+
+      if (errors) {
+        toast.remove()
+
+        Object.keys(errors).forEach((field) => {
+          errors[field].map((errMsg) => {
+            toast(errMsg, { icon: '😠', duration: 10000 })
+          })
+        })
+      } else {
+        toast('Something went wrong.', { icon: '😠', duration: 6000 })
+      }
+    },
+    onSuccess: () => {
+      router.replace('/')
+      toast('Document added.')
+    },
+  })
 
   function handleChange(file: File) {
     setFile(file)
@@ -38,8 +73,16 @@ function NewDocumentPage() {
     return string.length < 25 ? string : `${string.slice(0, 30)}...`
   }
 
-  function onSubmit(e: FormEvent<HTMLFormElement>) {
+  async function onSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault()
+
+    const data = { ...formData, file }
+
+    if (!!!data.description) {
+      delete data.description
+    }
+
+    newDocumentMutation.mutate(data)
   }
 
   const changeFormData = changeFormDataFactory<NewDocumentFormState>(setFormData)
@@ -54,6 +97,8 @@ function NewDocumentPage() {
         'aria-label': 'Name',
         value: formData.name,
         onChange: (e) => changeFormData('name', e.target.value),
+        maxLength: 255,
+        required: true,
       },
       {
         type: 'text',
@@ -63,6 +108,8 @@ function NewDocumentPage() {
         'aria-label': 'Tag',
         value: formData.tag,
         onChange: (e) => changeFormData('tag', e.target.value),
+        maxLength: 255,
+        required: true,
       },
     ],
   })
@@ -99,9 +146,14 @@ function NewDocumentPage() {
             aria-label="Description"
             value={formData.description}
             onChange={(e) => changeFormData('description', e.target.value)}
+            maxLength={2000}
           ></textarea>
 
-          <button type="submit" className="btn btn--secondary w-full">
+          <button
+            type="submit"
+            className="btn btn--secondary w-full disabled:cursor-wait"
+            disabled={newDocumentMutation.isPending}
+          >
             Create
           </button>
         </form>
